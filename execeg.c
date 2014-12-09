6518a5fea12c182e83cdc606a8f97fcd67ac4afc
Hari Caushik <hcaushik@gmail.com>
2014-12-08 23:50:02 -0800

    exec example c code

---
+++ execeg.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1) {
+		printf("Fork error\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (!pid) {
+		// child process
+		printf("I am the child process.\n");
+		execv("/usr/bin/python2.6", NULL);
+	}
+	else {
+		// parent process
+		printf("I am the parent process.\n");
+	}
+	wait();
+	return 1;
+}
